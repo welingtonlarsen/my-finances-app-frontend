7b1b2cd0f9421ab58ebeb15a5f97cf0f0ace79ab
welingtonlarsen <welingtonlarsen@pop-os.localdomain>
2024-01-09 22:46:33 -0300

    refactor: lint file

--- src/features/dashboard/create-expense-modal.jsx
+++ src/features/dashboard/create-expense-modal.jsx
@@ -1,79 +1,99 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { useLoaderData, useSubmit } from 'react-router-dom';
-import { Controller, useForm } from 'react-hook-form';
-import InputAdornment from '@mui/material/InputAdornment';
-import { Box } from '@mui/material';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useLoaderData } from "react-router-dom";
+import { Controller, useForm } from "react-hook-form";
+import InputAdornment from "@mui/material/InputAdornment";
+import { Box } from "@mui/material";
 
-const installments = [{id: 1, name: 1}, {id: 2, name: 2}, {id: 3, name: 3}, {id: 4, name: 4}, {id: 5, name: 5}, 
-  {id: 6, name: 6}, {id: 7, name: 7}, {id: 8, name: 8}, {id: 9, name: 9}, {id: 10, name: 10}, {id: 11, name: 11}, {id: 12, name: 12}]
+const installments = [
+  { id: 1, name: 1 },
+  { id: 2, name: 2 },
+  { id: 3, name: 3 },
+  { id: 4, name: 4 },
+  { id: 5, name: 5 },
+  { id: 6, name: 6 },
+  { id: 7, name: 7 },
+  { id: 8, name: 8 },
+  { id: 9, name: 9 },
+  { id: 10, name: 10 },
+  { id: 11, name: 11 },
+  { id: 12, name: 12 },
+];
 
-const SelectComponent = ({control, options, label, name}) => {
-  if(!options) return
-  return <FormControl variant="standard" sx={{minWidth: '100px'}}>
-    <InputLabel>{label}</InputLabel>
-    <Controller
-      name={name}
-      control={control}
-      render={({ field }) => (
-        <Select {...field} label={label}>
-          {options.map(option => (
-            <MenuItem key={option.id} value={option.id}>{option.name}</MenuItem>
-          ))}
-        </Select>
-      )}
-    />
-  </FormControl>
-}
+const SelectComponent = ({ control, options, label, name }) => {
+  if (!options) return;
+  return (
+    <FormControl variant="standard" sx={{ minWidth: "100px" }}>
+      <InputLabel>{label}</InputLabel>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <Select {...field} label={label}>
+            {options.map((option) => (
+              <MenuItem key={option.id} value={option.id}>
+                {option.name}
+              </MenuItem>
+            ))}
+          </Select>
+        )}
+      />
+    </FormControl>
+  );
+};
 
-export default function CreateExpenseModal({open, handleClose}) {
-  const data = useLoaderData()
-  let submit = useSubmit();
+export default function CreateExpenseModal({ open, handleClose }) {
+  const data = useLoaderData();
 
-  if(!data) return
+  if (!data) return;
 
-  const { control, handleSubmit, watch, formState: { errors } } = useForm({
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
       amount: undefined,
-      date: "2023-01-05",
       categoryId: 1,
       paymentMethodId: 1,
       installments: 2,
       currentInstallment: 2,
-      description: ""
-    }
+      description: "",
+    },
   });
 
-  const onSubmit = data => {
+  console.log(errors);
+  const onSubmit = (data) => {
     // set loading true
     // post data
     // set loading false
     // redirect to /dashboard
-    console.log(data)
+    console.log(errors);
   };
 
   return (
     <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Gasto</DialogTitle>
-        <DialogContent sx={{minWidth: '550px'}}>
-        <DialogContentText sx={{mb: '30px'}}>
-            Adicione o novo gasto.  
+      <DialogTitle>Gasto</DialogTitle>
+      <DialogContent sx={{ minWidth: "550px" }}>
+        <DialogContentText sx={{ mb: "30px" }}>
+          Adicione o novo gasto.
         </DialogContentText>
         <form onSubmit={handleSubmit(onSubmit)}>
-          
-          <Box sx={{display: 'flex', flexDirection: 'column'}}>
-            <Box sx={{mb: '30px'}}>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Box sx={{ mb: "30px" }}>
               <Controller
+                rules={{ required: "Data é obrigatório" }}
                 name="date"
                 control={control}
                 render={({ field: { onChange, onBlur, value, ref } }) => (
@@ -85,13 +105,21 @@ export default function CreateExpenseModal({open, handleClose}) {
                     onBlur={onBlur}
                     selected={value}
                     inputRef={ref}
-                    variant='standard'
+                    variant="standard"
                   />
                 )}
               />
             </Box>
-            <Box sx={{display: 'flex', alignItems: 'end', justifyContent: 'space-between', mb: '30px'}}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "end",
+                justifyContent: "space-between",
+                mb: "30px",
+              }}
+            >
               <Controller
+                rules={{ required: "Descrição é obrigatório", minLength: 5 }}
                 name="description"
                 control={control}
                 defaultValue=""
@@ -105,50 +133,76 @@ export default function CreateExpenseModal({open, handleClose}) {
                     InputLabelProps={{
                       shrink: true,
                     }}
-                    placeholder='Descrição'
+                    placeholder="Descrição"
                   />
                 )}
               />
-              <SelectComponent control={control} options={data.categories} label='Categoria' name='categoryId'/>
-              <SelectComponent control={control} options={data.paymentMethods} label='Pagamento' name='paymentMethodId'/>
-            </Box>
-            <Box sx={{display: 'flex', alignItems: 'end', justifyContent: 'space-between', mb: '30px', maxWidth: '450px'}}>
-              <Controller
-                  name="amount"
-                  control={control}
-                  defaultValue=""
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      id="amount"
-                      label="Valor"
-                      type="number"
-                      variant="standard"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                      InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="start">$</InputAdornment>
-                        ),
-                      }}
-                      placeholder='Valor'
-                      sx={{maxWidth: '205px'}}
-                    />
-                  )}
+              <SelectComponent
+                control={control}
+                options={data.categories}
+                label="Categoria"
+                name="categoryId"
               />
-              <SelectComponent control={control} options={installments} label='Parcela' name='currentInstallment'/>
-              <SelectComponent control={control} options={installments} label='Parcelas' name='installments'/>
+              <SelectComponent
+                control={control}
+                options={data.paymentMethods}
+                label="Pagamento"
+                name="paymentMethodId"
+              />
             </Box>
-          </Box>  
-
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "end",
+                justifyContent: "space-between",
+                mb: "30px",
+                maxWidth: "450px",
+              }}
+            >
+              <Controller
+                name="amount"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    id="amount"
+                    label="Valor"
+                    type="number"
+                    variant="standard"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">$</InputAdornment>
+                      ),
+                    }}
+                    placeholder="Valor"
+                    sx={{ maxWidth: "205px" }}
+                  />
+                )}
+              />
+              <SelectComponent
+                control={control}
+                options={installments}
+                label="Parcela"
+                name="currentInstallment"
+              />
+              <SelectComponent
+                control={control}
+                options={installments}
+                label="Parcelas"
+                name="installments"
+              />
+            </Box>
+          </Box>
         </form>
-
-        </DialogContent>
-        <DialogActions>
+      </DialogContent>
+      <DialogActions>
         <Button onClick={handleClose}>Cancel</Button>
         <Button onClick={handleSubmit(onSubmit)}>Subscribe</Button>
-        </DialogActions>
+      </DialogActions>
     </Dialog>
   );
 }
