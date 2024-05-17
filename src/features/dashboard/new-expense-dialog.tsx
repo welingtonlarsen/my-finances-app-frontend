37c3bd26902d6d6e87cb8facbe9d9c0ea71fe562
Welington Larsen <welington.larsen@luxuryescapes.com>
2024-05-17 10:41:24 -0300

    fix dialog overflow

--- src/features/dashboard/new-expense-dialog.tsx
+++ src/features/dashboard/new-expense-dialog.tsx
@@ -80,7 +80,7 @@ export function NewExpenseDialog() {
           <Plus className="h-4 w-4" />
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[425px] overflow-y-scroll max-h-screen">
         <DialogHeader>
           <DialogTitle>New expense</DialogTitle>
           <DialogDescription>Include a new expense to your list.</DialogDescription>
