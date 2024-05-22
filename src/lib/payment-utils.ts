896cc7b1c55db6f0a600951ad04f37e69bfcd426
Welington Larsen <welington.larsen@luxuryescapes.com>
2024-05-21 22:15:18 -0300

    fix png paths

--- src/lib/payment-utils.ts
+++ src/lib/payment-utils.ts
@@ -1,12 +1,12 @@
 export function getSVGOfPaymentMethod(paymentMethod: string) {
   switch (paymentMethod) {
     case 'PIX':
-      return '/public/payment/pix.png';
+      return '/payment/pix.png';
     case 'CASH':
-      return '/public/payment/money.png';
+      return '/payment/money.png';
     case 'CARD':
     case 'CREDIT_CARD':
-      return '/public/payment/card.png';
+      return '/payment/card.png';
     default:
       return '';
   }
