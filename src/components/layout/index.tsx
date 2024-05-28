914f08af8f3a1e866f9379067e6ccde6712e3057
Welington Larsen <welington.larsen@luxuryescapes.com>
2024-05-27 23:01:39 -0300

    set max width

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -165,7 +165,7 @@ export function Layout({ title, children }: TLayout) {
         </header>
 
         {/* Content */}
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 max-w-screen-2xl">
           {!!title && (
             <div className="flex items-center">
               <h1 className="text-lg font-semibold md:text-2xl">{title}</h1>
