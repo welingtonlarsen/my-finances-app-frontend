68b758832c9835d5027e0c6f42992d322252cd97
Welington Larsen <welington.larsen@luxuryescapes.com>
2024-10-02 14:24:53 -0300

    wip

--- src/components/layout/upgrade-card.tsx
+++ src/components/layout/upgrade-card.tsx
@@ -8,7 +8,7 @@ export default function UpgradeCard() {
     <Card>
       <CardHeader className="p-2 pt-0 md:p-4">
         <CardTitle>Upgrade to Pro</CardTitle>
-        <CardDescription>Unlock all features and get unlimited access to our support team.</CardDescription>
+        <CardDescription>Unlock new features and get unlimited access to our support team.</CardDescription>
       </CardHeader>
       <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
         <Button size="sm" className="w-full" disabled={true}>
